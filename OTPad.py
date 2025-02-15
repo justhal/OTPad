3dd8eec33c718eea7ac50a2a1918dd291d62c38b
justhal <justhal@gmail.com>
2025-02-15 13:48:54 -0500

    Encryption and decryption

--- OTPad.py
+++ OTPad.py
@@ -3,22 +3,43 @@ key = "SK"
 
 ciphertext = ""
 
+#print(ciphertext)
+
+def decrypt(ciphertext, key):
+    plaintext = ""
+    for c, k in zip(ciphertext, key):
+        plaintext += chr(ord(c) ^ ord(k))
+    return plaintext
+
+ciphertext = decrypt(plaintext, key)
+
+#decrypted = decrypt(ciphertext, key)
+
+#print(decrypted)
+
+decrypted = ""
+
 for index in range(len(plaintext)):
-    plaintext_character = plaintext[index]
+    plaintext_character = ciphertext[index]
     key_character = key[index]
     plaintext_integer = ord(plaintext_character)
     key_integer = ord(key_character)
     ciphertext_integer = plaintext_integer ^ key_integer
     ciphertext_character = chr(ciphertext_integer)
-    ciphertext = ciphertext_character + ciphertext 
+    decrypted = decrypted + ciphertext_character
 
-print(ciphertext)
+print(decrypted)
 
 # XOR Operation:
 # Plaintext 7   0111
 # Key 3         0011
 # ------------------
 # Ciphertext    0100
+
+# Plaintext     72      01001000
+# Key           83      01010011
+# Ciphertext    34      00011011
+
 
 
 
